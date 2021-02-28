556553d8b435be7643d1e551b0b02697fc0f6062
Kuriko Moe <kurikomoe@gmail.com>
2021-03-01 02:28:07 +0800

    using two key to sort

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var generator = require('./lib/generator');
 
+console.log("restful plugin loaded");
+
 hexo.extend.generator.register('restful', function(site) {
     return generator(Object.assign({}, hexo.config, hexo.theme.config), site);
 });
